92aec1603b832e56a8fde78987e1a914ecad3f2e
Owen Henley <owenhenley@me.com>
2019-04-27 14:05:11 +0100

    Fixed assets and setup logic to hold images.

--- Project_1/Project1/Project1/ViewController.swift
+++ Project_1/Project1/Project1/ViewController.swift
@@ -10,11 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private var photosAsString = [String]()
+
+    private let nssl = "nssl"
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupFileManager()
     }
 
+    /// Setup the file manager
+    ///
+    /// This is used to setup the file manager to access the images saved to our
+    /// Bundle resource path.
+    private func setupFileManager() {
+        let manager = FileManager.default
+        let path = Bundle.main.resourcePath! // iOS NEEDS a resourcepath, so we can force it. Pulling from project navigator, *not* assets.
+        let items = try! manager.contentsOfDirectory(atPath: path) // If we cant read the app bundle, teh app wont work anyway, so we can force try
 
+        for item in items {
+            if item.hasPrefix(nssl) {
+                photosAsString.append(item)
+            }
+        }
+        print(photosAsString)
+    }
 }
 
