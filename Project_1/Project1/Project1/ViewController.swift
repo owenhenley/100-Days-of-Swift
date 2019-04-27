d747fa0a6ae0c3c54999ff21d76cc80223a1ab3b
Owen Henley <owenhenley@me.com>
2019-04-27 14:23:13 +0100

    Implemented populating tableview with data

--- Project_1/Project1/Project1/ViewController.swift
+++ Project_1/Project1/Project1/ViewController.swift
@@ -8,18 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    private var photosAsString = [String]()
+class ViewController: UITableViewController {
 
     private let nssl = "nssl"
+    private let cellId = "tableViewCell"
+    private var photosAsString = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         setupFileManager()
     }
 
-    /// Setup the file manager
+    /// Setup the file manager.
     ///
     /// This is used to setup the file manager to access the images saved to our
     /// Bundle resource path.
@@ -33,7 +33,16 @@ class ViewController: UIViewController {
                 photosAsString.append(item)
             }
         }
-        print(photosAsString)
+    }
+
+    // MARK: - Setup table view
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return photosAsString.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+        cell.textLabel?.text = photosAsString[indexPath.row]
+        return cell
     }
 }
-
