5ce25a110370ec62c9544919024b9a82320515bf
Owen Henley <owenhenley@me.com>
2019-05-12 13:30:31 +0100

    Added models and json parser

---
+++ Project_7/Project_7/Petitions.swift
@@ -0,0 +1,13 @@
+//
+//  Petitions.swift
+//  Project_7
+//
+//  Created by Owen Henley on 12/05/2019.
+//  Copyright © 2019 Owen Henley. All rights reserved.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
--- Project_7/Project_7/ViewController.swift
+++ Project_7/Project_7/ViewController.swift
@@ -10,10 +10,28 @@ import UIKit
 
 class ViewController: UITableViewController {
 
-    private var petitions = [String]()
+    private var petitions = [Petition]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        fetchPetitions()
+    }
+
+    private func fetchPetitions() {
+        let urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        if let url = URL(string: urlString) {
+            if let data = try? Data(contentsOf: url) {
+                parse(json: data)
+            }
+        }
+    }
+
+    private func parse(json:Data) {
+        let decoder = JSONDecoder()
+        if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {
+            petitions = jsonPetitions.results
+            tableView.reloadData()
+        }
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -22,10 +40,10 @@ class ViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = "Title"
-        cell.detailTextLabel?.text = "Subtitle"
+        let petition = petitions[indexPath.row]
+        cell.textLabel?.text = petition.title
+        cell.detailTextLabel?.text = petition.body
         return cell
     }
-
 }
 
