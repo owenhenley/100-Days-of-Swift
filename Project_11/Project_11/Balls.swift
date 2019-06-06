fb2e6df126cb6145a7b767b83257abf0df3888bd
Owen Henley <owenhenley@me.com>
2019-06-06 20:13:48 +0100

    Challenge 3

---
+++ Project_11/Project_11/Balls.swift
@@ -0,0 +1,20 @@
+//
+//  Balls.swift
+//  Project_11
+//
+//  Created by Owen Henley on 06/06/2019.
+//  Copyright © 2019 Owen Henley. All rights reserved.
+//
+
+import Foundation
+
+enum Ball {
+    static let red = "ballRed"
+    static let blue = "ballBlue"
+    static let cyan = "ballCyan"
+    static let green = "ballGreen"
+    static let grey = "ballGrey"
+    static let purple = "ballPurple"
+    static let yellow = "ballYellow"
+    static let all = [red, blue, cyan, green, grey, purple, yellow]
+}
--- Project_11/Project_11/GameScene.swift
+++ Project_11/Project_11/GameScene.swift
@@ -7,13 +7,20 @@
 //
 
 import SpriteKit
-import Logging
+import UIKit
 
 class GameScene: SKScene {
 
     // MARK: - Properties
 
     private var scoreLabel: SKLabelNode!
+    private var ballLimit: Int = 15 {
+        didSet {
+            if ballLimit == 0 {
+                showGameOverAlert()
+            }
+        }
+    }
 
     private var score = 0 {
         didSet {
@@ -73,24 +80,27 @@ class GameScene: SKScene {
 
         let location = touch.location(in: self)
         let objects = nodes(at: location)
+
         guard let frame = view?.frame else {
             fatalError("There is no view")
         }
         let dropLimit = frame.height - 100
 
-        if location.y > dropLimit {
-            if objects.contains(editLabel) {
-                editingMode.toggle()
+        if objects.contains(editLabel) {
+            editingMode.toggle()
+        } else {
+            if editingMode {
+                let size = CGSize(width: Int.random(in: 16...128), height: 16)
+                let box = SKSpriteNode(color: UIColor(red: CGFloat.random(in: 0...1), green: CGFloat.random(in: 0...1), blue: CGFloat.random(in: 0...1), alpha: 1), size: size)
+                box.zRotation = CGFloat.random(in: 0...3)
+                box.position = location
+                box.physicsBody = SKPhysicsBody(rectangleOf: box.size)
+                box.physicsBody?.isDynamic = false
+                box.name = "box"
+                addChild(box)
             } else {
-                if editingMode {
-                    let size = CGSize(width: Int.random(in: 16...128), height: 16)
-                    let box = SKSpriteNode(color: UIColor(red: CGFloat.random(in: 0...1), green: CGFloat.random(in: 0...1), blue: CGFloat.random(in: 0...1), alpha: 1), size: size)
-                    box.zRotation = CGFloat.random(in: 0...3)
-                    box.position = location
-                    box.physicsBody = SKPhysicsBody(rectangleOf: box.size)
-                    box.physicsBody?.isDynamic = false
-                    addChild(box)
-                } else {
+                if location.y > dropLimit {
+                    self.isUserInteractionEnabled = false
                     let ball = randomBall()
                     ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.size.width / 2)
                     ball.physicsBody?.restitution = 0.5
@@ -144,28 +154,61 @@ class GameScene: SKScene {
         let spinForever = SKAction.repeatForever(spin)
         slotGlow.run(spinForever)
     }
+
+    private func showGameOverAlert() {
+        let ac = UIAlertController(title: "Game Over", message: "You ran out of balls. Your final score was: \(score)!", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Reset", style: .default, handler: (resetGame)))
+        self.view?.window?.rootViewController?.present(ac, animated: true)
+    }
+
+    @objc private func resetGame(action: UIAlertAction) {
+        ballLimit = 15
+        score = 0
+
+        self.children.forEach { node in
+            if let remainingBox = self.childNode(withName: "box") {
+                self.destroy(object: remainingBox)
+            }
+        }
+    }
 }
 
 // MARK: - SKPhysicsContactDelegate
 
 extension GameScene: SKPhysicsContactDelegate {
     func collison(between ball: SKNode, object: SKNode) {
+        if object.name == "box" {
+            destroy(object: object)
+            return
+        }
+
         if object.name == "good" {
-            destroy(ball: ball)
+            destroy(object: ball)
             score += 1
         } else if object.name == "bad" {
-            destroy(ball: ball)
+            destroy(object: ball)
             score -= 1
         }
+        ballLimit -= 1
+        self.isUserInteractionEnabled = true
     }
 
-    func destroy(ball: SKNode) {
-        if let fireParticles = SKEmitterNode(fileNamed: "FireParticles") {
-            fireParticles.position = ball.position
-            addChild(fireParticles)
+    func destroy(object: SKNode) {
+        switch object.name {
+        case "box":
+            if let magicParticles = SKEmitterNode(fileNamed: "MagicParticles") {
+                magicParticles.position = object.position
+                addChild(magicParticles)
+            }
+        default:
+            if let fireParticles = SKEmitterNode(fileNamed: "FireParticles") {
+                fireParticles.position = object.position
+                addChild(fireParticles)
+            }
         }
 
-        ball.removeFromParent()
+
+        object.removeFromParent()
     }
 
     func didBegin(_ contact: SKPhysicsContact) {
@@ -178,15 +221,4 @@ extension GameScene: SKPhysicsContactDelegate {
             collison(between: nodeB, object: nodeA)
         }
     }
-}
-
-enum Ball {
-    static let red = "ballRed"
-    static let blue = "ballBlue"
-    static let cyan = "ballCyan"
-    static let green = "ballGreen"
-    static let grey = "ballGrey"
-    static let purple = "ballPurple"
-    static let yellow = "ballYellow"
-    static let all = [red, blue, cyan, green, grey, purple, yellow]
 }
