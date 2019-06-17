ae763f29f35b2b7202cc545c35b91cd2a0fe17a4
Owen Henley <owenhenley@me.com>
2019-06-17 15:18:08 +0100

    Day 52

--- Project_13/Project_13/ViewController.swift
+++ Project_13/Project_13/ViewController.swift
@@ -10,11 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var intensitySlider: UISlider!
+    var currentImage: UIImage!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        title = "Instafilter"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(importPicture))
     }
 
+    @IBAction func intensityChanged(_ sender: Any) {
+    }
 
+    @IBAction func changeFilter(_ sender: Any) {
+    }
+
+    @IBAction func save(_ sender: Any) {
+    }
+}
+
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    @objc func importPicture() {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.editedImage] as? UIImage else { return }
+        dismiss(animated: true)
+        currentImage = image
+    }
 }
 
