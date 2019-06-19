a61207453d17e4f2fc413ffdc902be5e8f0bf2b1
Owen Henley <owenhenley@me.com>
2019-06-19 11:11:49 +0100

    challange 3

--- Project_13/Project_13/ViewController.swift
+++ Project_13/Project_13/ViewController.swift
@@ -13,6 +13,9 @@ class ViewController: UIViewController {
 
     @IBOutlet var imageView: UIImageView!
     @IBOutlet var intensitySlider: UISlider!
+    @IBOutlet var scaleSlider: UISlider!
+    @IBOutlet var radiusSlider: UISlider!
+    @IBOutlet var centerSlider: UISlider!
     var currentImage: UIImage!
 
     var context: CIContext!
@@ -91,11 +94,11 @@ extension ViewController: UIImagePickerControllerDelegate, UINavigationControlle
         let inputKeys = currentFilter.inputKeys
 
         if inputKeys.contains(kCIInputIntensityKey) { currentFilter.setValue(intensitySlider.value, forKey: kCIInputIntensityKey) }
-        if inputKeys.contains(kCIInputRadiusKey) { currentFilter.setValue(intensitySlider.value * 200, forKey: kCIInputRadiusKey) }
-        if inputKeys.contains(kCIInputScaleKey) { currentFilter.setValue(intensitySlider.value * 10, forKey: kCIInputScaleKey) }
+        if inputKeys.contains(kCIInputRadiusKey) { currentFilter.setValue(radiusSlider.value * 200, forKey: kCIInputRadiusKey) }
+        if inputKeys.contains(kCIInputScaleKey) { currentFilter.setValue(scaleSlider.value * 10, forKey: kCIInputScaleKey) }
         if inputKeys.contains(kCIInputCenterKey) { currentFilter.setValue(CIVector(x: currentImage.size.width / 2, y: currentImage.size.height / 2), forKey: kCIInputCenterKey) }
 
-        if let cgimg = context.createCGImage(currentFilter.outputImage!, from: currentFilter.outputImage!.extent) {
+     f   if let cgimg = context.createCGImage(currentFilter.outputImage!, from: currentFilter.outputImage!.extent) {
             let processedImage = UIImage(cgImage: cgimg)
             self.imageView.image = processedImage
         }
