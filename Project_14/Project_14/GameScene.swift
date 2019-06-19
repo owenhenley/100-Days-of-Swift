97afe8b68b4ca605c11e0fa909763d54a9a1f700
Owen Henley <owenhenley@me.com>
2019-06-19 16:41:13 +0100

    moving penguins

--- Project_14/Project_14/GameScene.swift
+++ Project_14/Project_14/GameScene.swift
@@ -17,6 +17,7 @@ class GameScene: SKScene {
     // MARK: - Properties
 
     var slots = [WhackSlot]()
+    var popupTime = 0.85
     var score = 0 {
         didSet {
             gameScore.text = "Score: \(score)"
@@ -43,6 +44,10 @@ class GameScene: SKScene {
         for i in 0..<4 { createSlot(at: CGPoint(x: 180 + (i * 170), y: 320)) }
         for i in 0..<5 { createSlot(at: CGPoint(x: 100 + (i * 170), y: 230)) }
         for i in 0..<4 { createSlot(at: CGPoint(x: 180 + (i * 170), y: 140)) }
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) { [weak self] in
+            self?.createEnemy()
+        }
     }
 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -54,5 +59,25 @@ class GameScene: SKScene {
         slot.configure(at: position)
         addChild(slot)
         slots.append(slot)
+    }
+
+    private func createEnemy() {
+        popupTime *= 0.991
+
+        slots.shuffle()
+        slots[0].show(hideTime: popupTime)
+
+        if Int.random(in: 0...12) > 4 { slots[1].show(hideTime: popupTime) }
+        if Int.random(in: 0...12) > 8 { slots[2].show(hideTime: popupTime) }
+        if Int.random(in: 0...12) > 10 { slots[3].show(hideTime: popupTime) }
+        if Int.random(in: 0...12) > 11 { slots[4].show(hideTime: popupTime) }
+
+        let minDelay = popupTime / 2
+        let maxDelay = popupTime * 2
+        let delay = Double.random(in: minDelay...maxDelay)
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + delay) { [weak self] in
+            self?.createEnemy()
+        }
     }
 }
