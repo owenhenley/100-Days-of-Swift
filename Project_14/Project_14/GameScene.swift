d9ec7083c19f07680fe5bad22acbd8cbead724cd
Owen Henley <owenhenley@me.com>
2019-06-19 11:44:39 +0100

    project 14

---
+++ Project_14/Project_14/GameScene.swift
@@ -0,0 +1,19 @@
+//
+//  GameScene.swift
+//  Project_14
+//
+//  Created by Owen Henley on 19/06/2019.
+//  Copyright © 2019 Owen Henley. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    override func didMove(to view: SKView) {
+
+    }
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+}
