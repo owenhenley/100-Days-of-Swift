03c0158a6fc3271d17ef5e3c0a2f2a485245cbdd
Owen Henley <owenhenley@me.com>
2019-09-30 08:26:48 +0100

    project 17 start

---
+++ Project_17/Project_17/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Project_17
+//
+//  Created by Owen Henley on 30/09/2019.
+//  Copyright © 2019 Owen Henley. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
